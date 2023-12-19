53661cd5f101c01591aa191dff09b55116931cb7
Michael Wu <mw885@cornell.edu>
2023-12-19 12:01:19 -0500

    Implement keyboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Canvas from "./components/Canvas";
 import Home from "./components/Home";
+import './App.css'
 import './index.css'
 
 function App() {
   return (
-    <div>
-      <h1 className="text-7xl text-center text-blue-400"> Hello world </h1>
+    <div className="root h-screen w-screen">
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 function Home() {
+
   return (
-    <div class=''>Home</div>
+    <div class=''>
+
+      <h1 className="text-7xl text-center text-blue-400"> Hello world </h1>
+      <KeyBoard />
+    </div>
   )
+}
+
+const KeyBoard = () => {
+  const [pressedKey, setPressedKey] = useState('');
+
+  const handleKeyDown = (e) => {
+    setPressedKey(e.key.toUpperCase());
+    console.log(e.key.toUpperCase());
+  }
+
+  const handleKeyUp = (e) => {
+    setPressedKey("");
+  }
+
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  })
+
+  const firstRow = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P']
+  const secondRow = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L']
+  const thirdRow = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+  const keyboard = [firstRow, secondRow, thirdRow]
+  return (
+    <div>
+      <div> Last pressed key: {pressedKey} </div>
+      <div>
+        {keyboard.map((row, rowIndex) => (
+          <div key={rowIndex} className='flex flex-row space-x-4 m-2 justify-center'>
+            {row.map((key, keyIndex) => (
+              <button key={keyIndex} className={`ash-gray rounded p-2 w-12 h-12 key-button ${key == pressedKey ? 'pressed' : ''}`}>
+                {key}
+              </button>
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home
